b313d26fb8291f38924cffecf96d805f16ffc848
EmonMridha <emonmridha712@gmail.com>
2025-03-08 10:33:11 +0600

    firs commit

---
+++ assignment.js
@@ -0,0 +1,24 @@
+document.getElementById('complete').addEventListener('click', function(){
+    alert('Board updated Successfully')
+})
+document.getElementById('complete2').addEventListener('click', function(){
+    alert('Board updated Successfully')
+})
+document.getElementById('complete3').addEventListener('click', function(){
+    alert('Board updated Successfully')
+})
+document.getElementById('complete4').addEventListener('click', function(){
+    alert('Board updated Successfully')
+})
+document.getElementById('complete5').addEventListener('click', function(){
+    alert('Board updated Successfully')
+})
+document.getElementById('complete6').addEventListener('click', function(){
+    alert('Board updated Successfully')
+})
+document.getElementById('complete6').addEventListener('click', function(){
+    const var1= document.getElementById('number').innerText;
+    const convertedNumber = parseInt(var1);
+})
+
+
